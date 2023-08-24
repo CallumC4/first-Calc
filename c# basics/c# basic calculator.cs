47f88436bde60032ccab28d0273571c046129980
CallumCunningham <CallumCunningham@LAPTOP-92EDPPHP>
2023-08-24 09:04:55 +0100

    Add project files.

---
+++ c# basics/c# basic calculator.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("choose a number");
+float num1 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("choose an operation");
+Console.WriteLine("choose 1 for addition");
+Console.WriteLine("choose 2 for subtraction");
+Console.WriteLine("choose 3 for multiplication");
+Console.WriteLine("choose 4 for division");
+
+int operation = int.Parse(Console.ReadLine());
+float result = 0;
+
+Console.WriteLine("choose another number");
+float num2 = float.Parse(Console.ReadLine());
+
+if (operation == 1)
+{ 
+    result = num1 + num2;
+}
+
+
+if (operation == 2)
+{
+     result = num1 - num2;
+}
+
+if (operation == 3)
+{
+     result = num1 * num2;
+}
+
+else
+{
+    result = num1 / num2;
+}
+
+Console.WriteLine(result);
+
+
+
+
